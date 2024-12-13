c9900b5f9fa32b2b0686587a8125877f4f04ba58
TaherCCG <taher.ccg@gmail.com>
2024-12-13 17:23:14 +0000

    Implement recipe upload functionality with form handling and AJAX requests; add AddRecipeView for UI interactions

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'eae6d5e0-f595-42f9-b54b-8ddf631ac202';
+export const MODAL_CLOSE_SEC = 2.5;
